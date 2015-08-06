c937696c8a2a9929f416a35a05b047bf92980b28
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-08-06 19:23:59 +0200

    Fix.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express();
 var port = process.env.PORT || 5000;
 var browserify = require("browserify");
 var babelify = require("babelify");
-var es6ify = require("es6ify");
+// var es6ify = require("es6ify");
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.json({limit: '3mb'}));
